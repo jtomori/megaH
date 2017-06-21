5beebabb4f15b767ef205ea02f86092322fb75b3
jtomori <tomori.juraj@gmail.com>
2017-06-21 19:53:44 +0000

    getting in first functions

--- houdini/scripts/python/megaH.py
+++ houdini/scripts/python/megaH.py
@@ -1,2 +1,21 @@
-def test():
-	print "testaassss"
+import glob, os
+
+# flattens down list of lists
+def flatten(A):
+    rt = []
+    for i in A:
+        if isinstance(i,list): rt.extend(flatten(i))
+        else: rt.append(i)
+    return rt
+
+# return list of folders inside specified folder
+def getFoldersPaths(path):
+	folders = [x[0] for x in os.walk(path)]
+	del folders[0]
+	return folders
+
+# return list of files matching mask inside specified folder
+def getFilesByMask(path, mask):
+	os.chdir(path)
+	lods = [file for file in glob.glob(mask)]
+	return lods

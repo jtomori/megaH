ba3305d192928d6cd1d4008189c5b8635acebefb
jtomori <tomori.juraj@gmail.com>
2018-05-04 20:21:12 +0200

    moved convenience functions to Utils class, dumped objCrack module and functions, added comments, changed file extension handling in ProcessAssets class

--- houdini/scripts/python/megaH.py
+++ houdini/scripts/python/megaH.py
@@ -1,91 +1,39 @@
 import glob, os, time, json, fnmatch
-import objCrack
 import hou
 
-# flattens down list of lists
-def flatten(A):
-    rt = []
-    for i in A:
-        if isinstance(i,list): rt.extend(flatten(i))
-        else: rt.append(i)
-    return rt
+# a class containing convenience methods
+class Utils(object):
+	# flattens down list of lists
+	@staticmethod	
+	def flatten(A):
+		rt = []
+		for i in A:
+			if isinstance(i,list): rt.extend(Utils.flatten(i))
+			else: rt.append(i)
+		return rt
 
-# return list of all folders inside specified folder, note that first list entry is "path" folder itself
-def getFoldersPaths(path):
-	folders = [x[0] for x in os.walk(path)]
-	return folders
+	# return list of all folders inside specified folder, note that first list entry is "path" folder itself
+	@staticmethod	
+	def getFoldersPaths(path):
+		folders = [x[0] for x in os.walk(path)]
+		return folders
 
-# return list of files matching mask inside specified folder
-def getFilesByMask(path, mask):
-	os.chdir(path)
-	lods = [file for file in glob.glob("*") if fnmatch.fnmatchcase(file, mask)]
-	return lods
+	# return list of files matching mask inside specified folder
+	@staticmethod	
+	def getFilesByMask(path, mask):
+		os.chdir(path)
+		lods = [file for file in glob.glob("*") if fnmatch.fnmatchcase(file, mask)]
+		return lods
 
-def getFilesRecursivelyByMask(path, mask):
-	matches = []
-	for root, dirnames, filenames in os.walk(path):
-		for filename in fnmatch.filter(filenames, mask):
-			matches.append(os.path.join(root, filename))
-	
-	return matches
-
-# multithreaded cracking of all OBJs in specified folder, function is calling multiple python processes, which are calling objCrack.crackMulti() function, but each process has different arguments
-def crackAllObjs(path):
-	import multiprocessing as multi
-	from threading import Thread
-	import inspect
-
-	# list containing Thread objects
-	threadsList = []
-	# number of threads
-	threads = multi.cpu_count() - 1
-
-	folders = getFoldersPaths(path)
-	objPaths = [ [os.path.join(folder, file) for file in getFilesByMask(folder, "*.obj")] for folder in folders]
-	objPaths = flatten(objPaths)
-
-	# split into even chunks
-	def chunkify(lst,n):
-		return [ lst[i::n] for i in xrange(n) ]
-	objPathsParts = chunkify(objPaths, threads)
-
-	# a function to be called in parallel, which is calling separate processes
-	def callProcess(pathsParts, index):
-		command = """python -c "import objCrack; objCrack.crackMulti(%s);" """ % (str(objPathsParts[index]) )
-
-		if os.name == "nt":
-			import subprocess
-			CREATE_NO_WINDOW = 0x08000000
-			subprocess.call(command, creationflags=CREATE_NO_WINDOW)
-		else:
-			os.system(command)
-
-	# go to the folder of this file, because of later module importing, this is because getFilesByMask() function is changing current director
-	curFolder = os.path.split( os.path.normpath(inspect.stack()[0][1]) )[0]
-	os.chdir(curFolder)
-
-	# spawn all threads
-	for x in xrange(threads):
-		t = Thread(target=callProcess, args=(objPathsParts, x))
-		threadsList.append(t)
-		t.start()
-
-	# wait for all threads to end
-	for x in threadsList: 
-		x.join()
-
-# a function to be called from Houdini, making use of Houdini dialogs
-# selectFile() does not seem to work on folders anymore, readInput() is usued instead
-def crackAllObjsHou():
-	#libPath = hou.getenv("MEGA_LIB")
-	#path = hou.ui.selectFile(start_directory=libPath, title="Select a folder containing assets to process", collapse_sequences=False, pattern="*.obj", chooser_mode=hou.fileChooserMode.Read)
-	choice, path = hou.ui.readInput("Enter folder path of Megascans library to convert, this operation can take some time.", buttons=('Convert','Cancel'), close_choice=1, initial_contents=hou.getenv("MEGA_LIB"))
-	path = os.path.normpath(path)
-	if choice == 0:
-		start = time.time()
-		crackAllObjs(path)
-		end = time.time()
-		hou.ui.displayMessage("OBJs cracking is done\nelapsed time: %0.2f seconds" % (end-start), title="Done")
+	# returns a list of files recursively found in a folder and matching a pattern
+	@staticmethod	
+	def getFilesRecursivelyByMask(path, mask):
+		matches = []
+		for root, dirnames, filenames in os.walk(path):
+			for filename in fnmatch.filter(filenames, mask):
+				matches.append(os.path.join(root, filename))
+		
+		return matches
 
 # generates and writes a dictionary with hierarchy of all megascan packs, assets their LODs
 class BuildHierarchy(object):
@@ -98,7 +46,7 @@ class BuildHierarchy(object):
 
 		# returns True if asset and LOD are reversed
 		def checkReverse(self, packs, pack):
-			assetName = getFilesByMask( os.path.join(self.libPath, packs[pack]), self.extMask )[0]
+			assetName = Utils.getFilesByMask( os.path.join(self.libPath, packs[pack]), self.extMask )[0]
 			assetName = assetName.split(".")[0].split("_")[-1]
 			if assetName.isdigit():
 				correct = False
@@ -108,7 +56,7 @@ class BuildHierarchy(object):
 
 		# returns list of assets in a pack
 		def getAssets(self, packs, pack):
-			assets = getFilesByMask( os.path.join(self.libPath, packs[pack]), self.extMask )
+			assets = Utils.getFilesByMask( os.path.join(self.libPath, packs[pack]), self.extMask )
 			if self.checkReverse(packs,  pack):
 				assets = [ asset.split(".")[0].split("_")[-2] for asset in assets ]
 			else:
@@ -118,7 +66,7 @@ class BuildHierarchy(object):
 
 		# returns dictionary of LODs per asset in pack as keys and full paths as values
 		def getLods(self, packs, pack, assets, asset):
-			lods = getFilesByMask( os.path.join(self.libPath, packs[pack]), "*" + assets[asset] + "*" + self.extMask)
+			lods = Utils.getFilesByMask( os.path.join(self.libPath, packs[pack]), "*" + assets[asset] + "*" + self.extMask)
 			if self.checkReverse(packs,  pack):
 				lods = { lod.split(".")[0].split("_")[-1] : os.path.join(packs[pack], lod) for lod in lods }
 			else:
@@ -127,7 +75,7 @@ class BuildHierarchy(object):
 
 		# build a dictionoary and save it to json file		
 		def build(self):
-			packs = getFoldersPaths(self.libPath)
+			packs = Utils.getFoldersPaths(self.libPath)
 			del packs[0] # removes folder itself as it is not expected to contain any assets geometry, it should contain only assets folders and json
 			packs = [x.split( os.path.sep )[-1] for x in packs]
 
@@ -168,7 +116,7 @@ class MegaLoad(object):
 	def packsList(self):
 		packs = [pack.encode("ascii") for pack in self.assetsIndex]
 		packs = [[packs[x], packs[x].replace("_", " ")] for x in xrange(len(packs))]
-		packs = flatten(packs)
+		packs = Utils.flatten(packs)
 		return packs
 
 	# finds assets in pack based on idexed file, outputs houdini menu-style list
@@ -180,7 +128,7 @@ class MegaLoad(object):
 		assets = self.assetsIndex[pack].keys()
 		assets = [x.encode("ascii") for x in assets]
 		assets = [ [x,x] for x in assets]
-		assets = flatten(assets)
+		assets = Utils.flatten(assets)
 		return assets
 
 	# finds LODs in asset in pack based on idexed file, outputs houdini menu-style list
@@ -197,7 +145,7 @@ class MegaLoad(object):
 		lods = [x.encode("ascii") for x in lods]
 		paths = [self.assetsIndex[pack][asset][lod].encode("ascii") for lod in lods]
 		lodsMenu = [ [ os.path.join(self.libPath, paths[n]) , lods[n] ] for n in xrange(len(lods))]
-		lodsMenu = flatten(lodsMenu)
+		lodsMenu = Utils.flatten(lodsMenu)
 		return lodsMenu
 
 	# checks checkbox in asset, if set, it will rename current node by asset name and LOD, it should be bound to callback of a load button (which might by hidden)
@@ -232,27 +180,26 @@ class MegaLoad(object):
 
 		node.parm("shader").set(shader)
 
+# a class managing cracking process
 class ProcessAssets(object):
+	# gets a path, extension from param from parent and replaces extension
 	@staticmethod
 	def convertInFilePath(node):
+		extension = ".bgeo.sc"
 		parent_node = node.parent()
 
 		try:
 			in_path = parent_node.parm("file").unexpandedString()
+			in_ext = parent_node.parent().parent().parm("ext").eval()
 		except AttributeError:
-			raise AttributeError("'file' parameter not found")
-
-		out_path = in_path
-
-		if in_path.endswith(".obj"):
-			out_path = in_path.replace(".obj", ".bgeo.sc")
-		elif in_path.endswith(".fbx"):
-			out_path = in_path.replace(".fbx", ".bgeo.sc")
-			
+			raise AttributeError("'file' or 'ext' parameter not found")
+		
+		out_path = in_path.replace(in_ext, extension)
 		out_path = hou.expandString(out_path)
 
 		return out_path
 
+	# generates child Process Asset SOP nodes and sets parameter, also creates Fetch ROPs pointing to them and merges them together
 	@staticmethod
 	def generateProcessAssetsNodes(node):
 		try:
@@ -265,7 +212,7 @@ class ProcessAssets(object):
 		ropnet = node.glob("ropnet")[0]
 		rop_merge = ropnet.glob("merge_render_all")[0]
 		
-		files = getFilesRecursivelyByMask(root_path, ext)
+		files = Utils.getFilesRecursivelyByMask(root_path, "*"+ext)
 		process_nodes = []
 		fetch_nodes = []
 
@@ -284,6 +231,7 @@ class ProcessAssets(object):
 		sopnet.layoutChildren()
 		ropnet.layoutChildren()
 	
+	# deletes generated child nodes
 	@staticmethod
 	def cleanChildren(node):
 		sopnet = node.glob("sopnet")[0]

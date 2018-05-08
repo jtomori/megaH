93b02cc564e58f85dec2f5d9c53e81eeb93b47f2
jtomori <tomori.juraj@gmail.com>
2018-05-08 14:56:56 +0200

    switched to docstrings, added relative paths to index, enabled recursive indexing of folders, added option for deleting cache in hou session after re-indexing, started work on updating params, but textures part needs to be reworked

--- houdini/scripts/python/megaH.py
+++ houdini/scripts/python/megaH.py
@@ -10,33 +10,43 @@ import fnmatch
 logging.basicConfig(level=logging.DEBUG) # set to logging.INFO to disable DEBUG logs
 log = logging.getLogger(__name__)
 
-# a class containing convenience methods
 class Utils(object):
-	# flattens down list of lists
+	"""
+	class containing convenience methods
+	"""
 	@staticmethod	
 	def flatten(A):
+		"""
+		flattens down list of lists
+		"""
 		rt = []
 		for i in A:
 			if isinstance(i,list): rt.extend(Utils.flatten(i))
 			else: rt.append(i)
 		return rt
 
-	# return list of all folders inside specified folder, note that first list entry is "path" folder itself
 	@staticmethod	
 	def getFoldersPaths(path):
+		"""
+		return list of all folders inside specified folder, note that first list entry is "path" folder itself
+		"""
 		folders = [x[0] for x in os.walk(path)][1:] # skips first entry which is path itself
 		return folders
 
-	# return list of files matching mask inside specified folder
 	@staticmethod	
 	def getFilesByMask(path, mask):
+		"""
+		return list of files matching mask inside specified folder
+		"""
 		os.chdir(path)
 		lods = [file for file in glob.glob("*") if fnmatch.fnmatchcase(file, mask)]
 		return lods
 
-	# returns a list of files recursively found in a folder and matching a pattern
 	@staticmethod	
 	def getFilesRecursivelyByMask(path, mask):
+		"""
+		returns a list of files recursively found in a folder and matching a pattern
+		"""
 		matches = []
 		for root, dirnames, filenames in os.walk(path):
 			for filename in fnmatch.filter(filenames, mask):
@@ -44,19 +54,36 @@ class Utils(object):
 		
 		return matches
 
-# generates and writes a dictionary with hierarchy of all megascans assets and their respective LODs
+	@staticmethod
+	def getLeafFoldersPaths(path):
+		"""
+		returns a list of recursively found leaf folders paths (the ones which do not contain any folders) in specified path
+		"""
+		folders = []
+		for root, dirs, files in os.walk(path):
+			if not dirs:
+				folders.append(root)
+		return folders
+
 class BuildAssetsHierarchy(object):
-	# init function, creates needed variables, config
+	"""
+	generates and writes a dictionary with hierarchy of all megascans assets and their respective LODs
+	"""
 	def __init__(self, ):
+		"""
+		init function, creates needed variables, config
+		"""
 		self.libPath = hou.getenv("MEGA_LIB") # a path pointing to root folder with assets to be indexed
 		self.libPath = os.path.normpath(self.libPath) # normalize it just to be sure
-		self.libPath = os.path.join(self.libPath, "3d") # append 3d folder which contains actual geometry
+		self.libPath_3d = os.path.join(self.libPath, "3d") # append 3d folder which contains actual geometry
 		self.extMask = "*.bgeo.sc" # extension of files (converted) to be indexed
-		self.libHierarchyJson = os.path.join(self.libPath, "index.json") # a path to output file with indexed data
+		self.libHierarchyJson = os.path.join(self.libPath_3d, "index.json") # a path to output file with indexed data
 	
-	# build a dict containing all the assets and needed information
 	def build(self, debug=False):
-		asset_folders_paths = Utils.getFoldersPaths(self.libPath)
+		"""
+		build a dict containing all the assets and needed information
+		"""
+		asset_folders_paths = Utils.getLeafFoldersPaths(self.libPath_3d)
 		asset_folders_names = []
 		
 		# create a dictionary with folder names as keys and folder full paths as values
@@ -75,7 +102,15 @@ class BuildAssetsHierarchy(object):
 			
 			# move the whole dict into another dict - for the future, it will enable to store more information without breaking the tool
 			asset_dict = {"assets" : lods_dict}
-			asset_dict["path"] = folder_path
+			asset_dict["path"] = folder_path.replace( self.libPath, "$MEGA_LIB", 1 )
+
+			# preview image
+			preview_image = Utils.getFilesByMask(value, "*Preview*")
+			if len(preview_image) == 0:
+				preview_image = ""
+			else:
+				preview_image = preview_image[0]
+			asset_dict["preview_image"] = preview_image
 
 			# get metadata from accompanying json file
 			environment = {}
@@ -102,24 +137,37 @@ class BuildAssetsHierarchy(object):
 			with open(self.libHierarchyJson, 'w') as out:
 				json.dump(index_dict, out, indent=2, sort_keys=True, ensure_ascii=False)
 	
-	# indexes all the assets and displays time information, ALT+Click for debug mode (printing to stdout instead of a file)
 	def buildAssetsHierarchyHou(self, kwargs):
+		"""
+		indexes all the assets and displays time information, ALT+Click for debug mode (printing to stdout instead of a file)
+		"""
 		start = time.time()
+		# recognize Alt+Click
 		if kwargs["altclick"]:
 			debug = True
 		else:
 			debug = False
 		self.build(debug)
+
+		# delete cache from hou.session
+		try:
+			log.debug("Deleting library index cache from hou.session")
+			del hou.session.__dict__["mega_index"]
+		except KeyError:
+			log.debug("Hou.session has no library index cache, not deleting anything")
+		
 		end = time.time()
 		hou.ui.displayMessage("Assets indexing done in: %0.4f seconds" % (end-start), title="Done")
 
-# a class implementing functionality of mega load digital asset
 class MegaLoad(object):
+	"""
+	class implementing functionality of mega load digital asset
+	"""
 	def __init__(self, force_recache=False):
 		self.libPath = hou.getenv("MEGA_LIB") # a path pointing to root folder with assets to be indexed
-		self.libPath = os.path.normpath(self.libPath) # normalize it just to be sure		
-		self.libPath = os.path.join(self.libPath, "3d") # append 3d folder which contains actual geometry
-		self.libHierarchyJson = os.path.join(self.libPath, "index.json") # a path to output file with indexed data
+		self.libPath = os.path.normpath(self.libPath) # normalize it just to be sure
+		self.libPath_3d = os.path.join(self.libPath, "3d") # append 3d folder which contains actual geometry
+		self.libHierarchyJson = os.path.join(self.libPath_3d, "index.json") # a path to output file with indexed data
 		self.shader = ""
 
 		# cache loaded index into hou.session, if re-indexed, it needs to be re-created
@@ -133,15 +181,19 @@ class MegaLoad(object):
 		
 		self.assetsIndex = hou.session.mega_index
 	
-	# returns a houdini-menu style list of assets
 	def assetMenuList(self):
+		"""
+		returns a houdini-menu style list of assets
+		"""
 		keys = self.assetsIndex.keys() # get all keys form index dictionary, sorted
 		keys.sort()
 		keys = [str(x) for pair in zip(keys,keys) for x in pair] # duplicate all elements, for houdini menu
 		return keys
 
-	# returns a houdini-menu style list of LODs for selected asset
 	def lodMenuList(self, node=None):
+		"""
+		returns a houdini-menu style list of LODs for selected asset
+		"""
 		if node == None:
 			node = hou.pwd()
 
@@ -163,27 +215,77 @@ class MegaLoad(object):
 		return menu_lods
 
 	def updateParms(self):
+		"""
+		updates mega load parameters with asset paths, lods, textures and stuff
+		"""
 		node = hou.pwd()
+		relative_enable = node.parm("paths_relative_enable").eval()
 
-		# get selected asset
+		# get selected asset, lod and display lod from node parameters
 		asset_number = node.parm("asset").eval()
 		asset_items = node.parm("asset").menuItems()
 		asset_lod_number = node.parm("asset_lod").eval()
 		asset_lod_items = node.parm("asset_lod").menuItems()
+		asset_lod_labels = node.parm("asset_lod").menuLabels()
+		display_lod_number = node.parm("display_lod_level").eval()
+		display_lod_items = node.parm("display_lod_level").menuItems()
 
-		lod = asset_lod_items[asset_lod_number]
 		asset = asset_items[asset_number]
+		lod = asset_lod_items[asset_lod_number]
+		lod_label = asset_lod_labels[asset_lod_number]
+		dispaly_lod = display_lod_items[display_lod_number]
 
 		asset_dict = self.assetsIndex[asset]
 
+		# determine asset and asset display paths
 		folder_path = asset_dict["path"]
-		file_path = os.path.join(folder_path, lod)
-		file_path = file_path.replace( self.libPath, "$MEGA_LIB", 1 )
+		asset_path = os.path.join(folder_path, lod)
+		asset_display_path = os.path.join(folder_path, dispaly_lod)
+		if not relative_enable:
+			asset_path = asset_path.replace( "$MEGA_LIB", self.libPath, 1 )
+			asset_display_path = asset_display_path.replace( "$MEGA_LIB", self.libPath, 1 )			
 
-		print file_path
+		# determine asset lod number
+		asset_lod_number = "High" if lod_label == "High" else lod_label
 
-	# checks checkbox in asset, if set, it will rename current node by asset name and LOD, it should be bound to callback of a load button (which might by hidden)
+		# determine texture paths
+		texture_resolution = node.parm("texture_resolution").eval()
+		texture_extension = node.parm("texture_extension").eval()
+		texture_extension_displacement = node.parm("texture_extension_displacement").eval()
+
+		tex_path_base = "_".join( asset_path.split("_")[:-1] ) + "_"
+		tex_path_base = tex_path_base + texture_resolution
+
+		albedo = tex_path_base + "_" + "Albedo" + texture_extension
+		bump = tex_path_base + "_" + "Bump" + texture_extension
+		cavity = tex_path_base + "_" + "Cavity" + texture_extension
+		fuzz = tex_path_base + "_" + "Fuzz" + texture_extension
+		roughness = tex_path_base + "_" + "Roughness" + texture_extension
+		specular = tex_path_base + "_" + "Specular" + texture_extension
+		normal = tex_path_base + "_" + "Normal" + "_" + asset_lod_number + texture_extension
+		normal = normal if asset_lod_number != "High" else ""
+		normalBump = tex_path_base + "_" + "NormalBump" + texture_extension
+		displacement = tex_path_base + "_" + "Displacement" + texture_extension_displacement
+
+		# update parameters
+		node.parm("asset_path").set(asset_path)
+		node.parm("asset_display_path").set(asset_display_path)
+		node.parm("asset_lod_number").set(asset_lod_number)
+
+		node.parm("albedo").set(albedo)
+		node.parm("bump").set(bump)
+		node.parm("cavity").set(cavity)
+		node.parm("fuzz").set(fuzz)
+		node.parm("roughness").set(roughness)
+		node.parm("specular").set(specular)
+		node.parm("normal").set(normal)
+		node.parm("normalBump").set(normalBump)
+		node.parm("displacement").set(displacement)
+
 	def autoRename(self):
+		"""
+		checks checkbox in asset, if set, it will rename current node by asset name and LOD, it should be bound to callback of a load button (which might by hidden)
+		"""
 		node = hou.pwd()
 		currentName = node.name()
 		enabled = node.evalParm("rename_node")
@@ -200,12 +302,13 @@ class MegaLoad(object):
 
 		if enabled and (currentName != newName):
 			node.setName(newName, unique_name=True)
-	
-	# apply asset and lod selection into parameter values and rename node
+
 	def apply(self):
+		"""
+		apply asset and lod selection into parameter values and rename node
+		"""
 		self.updateParms()
 		self.autoRename()
-
 
 # a class covering functionality of jt_megaLoad digital asset
 class MegaLoadOld(object):
@@ -286,11 +389,15 @@ class MegaLoadOld(object):
 
 		node.parm("shader").set(shader)
 
-# a class managing cracking process
 class ProcessAssets(object):
-	# gets a path, extension from param from parent and replaces extension
+	"""
+	a class managing cracking process
+	"""
 	@staticmethod
 	def convertInFilePath(node):
+		"""
+		gets a path, extension from param from parent and replaces extension
+		"""
 		extension = ".bgeo.sc"
 		parent_node = node.parent()
 
@@ -305,9 +412,11 @@ class ProcessAssets(object):
 
 		return out_path
 
-	# generates child Process Asset SOP nodes and sets parameter, also creates Fetch ROPs pointing to them and merges them together
 	@staticmethod
 	def generateProcessAssetsNodes(node):
+		"""
+		generates child Process Asset SOP nodes and sets parameter, also creates Fetch ROPs pointing to them and merges them together
+		"""
 		try:
 			root_path = node.parm("folder").eval()
 			ext = node.parm("ext").eval()
@@ -337,9 +446,11 @@ class ProcessAssets(object):
 		sopnet.layoutChildren()
 		ropnet.layoutChildren()
 	
-	# deletes generated child nodes
 	@staticmethod
 	def cleanChildren(node):
+		"""
+		deletes generated child nodes
+		"""
 		sopnet = node.glob("sopnet")[0]
 		ropnet = node.glob("ropnet")[0]
 		nodes = sopnet.glob("*") + ropnet.glob("* ^merge_render_all")
